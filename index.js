e493f153ae279b260e6e9b16351580fcc3fe1874
Vincenzo <vincebshock@gmail.com>
2021-03-13 17:58:59 +0100

    ➕cors dep added for local testing

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 'use strict';
 const express = require('express');
+var cors = require('cors');
+
 const audiobooksRoute = require('./routes/audiobooksRoute');
 const audiotracksRoute = require('./routes/audiotracksRoute');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // use router
+app.use(cors());
 app.use('/audiobooks', audiobooksRoute);
 app.use('/audiotracks', audiotracksRoute);
 
